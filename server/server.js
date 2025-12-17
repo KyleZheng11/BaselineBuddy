193d0cc5f1ea709ec05ce754c2dc048c3f18a5bf
KyleZheng11 <k1105yle@gmail.com>
2025-12-16 20:06:39 -0500

    f

--- server/server.js
+++ server/server.js
@@ -1,8 +1,19 @@
 const express = require('express')
+const pool = require('./db')
 const app = express()
 
 app.get("/api", (req, res) => {
     res.json({ "users": ["userOne", "userTwo", "userThree", "userFour"] })
+})
+
+app.get('/api/test-db', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.json({ success: true, time: result.rows[0] });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Database connection failed' });
+  }
 })
 
 app.listen(5000, () => {console.log("Server started on port 5000") })
